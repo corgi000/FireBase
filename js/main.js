e48ecf137359d51745cd8090f9046518fc24ffbc
corgi000 <hiro_taka1980@yahoo.co.jp>
2017-05-25 21:26:23 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // GoogleCouldMessagingで取得したAPIキーを設定
-var API_KEY = 'AIzaSyDpx5LWS0vpR_PfHJ4tMdWBwV9JR-N4QiE';
+var API_KEY = 'AIzaSyBgB4yegLrkLE-tM0XA4sYI57X3ZnOaACk';
 
 // GCMのエンドポイントのBaseURL
 var GCM_ENDPOINT = 'https://android.googleapis.com/gcm/send';
